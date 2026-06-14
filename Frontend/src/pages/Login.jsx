8fe52d659a5dbbc5b0025da632b9cc19bb95ba46
Dhanush <dhanushkadiyam01@gmail.com>
2026-06-14 18:59:35 +0530

    Improved authentication UI and validation(Register and Login page updated)

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import api from "../services/api";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./Auth.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
+      setLoading(true);
       const response = await api.post("/auth/login", {
         email,
         password,
@@ -19,45 +22,56 @@ function Login() {
 
       console.log(response.data);
       localStorage.setItem("token", response.data.token);
+      setLoading(false);
       navigate("/dashboard");
     } catch (error) {
       console.log(error.response?.data);
+      setError("Invalid email or password");
+      setLoading(false);
     }
   };
 
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1 className="auth-title">Login</h1>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
+        <form onSubmit={handleSubmit}>
+          <div className="auth-form-group">
+            <label>Email</label>
+            <br />
+            <input
+              className="auth-input"
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
           <br />
-          <input
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
 
-        <br />
-
-        <div>
-          <label>Password</label>
+          <div className="auth-form-group">
+            <label>Password</label>
+            <br />
+            <input
+              className="auth-input"
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {error && <p className="auth-error">{error}</p>}
           <br />
-          <input
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <br />
-
-        <button type="submit">Login</button>
-      </form>
+          <button type="submit" className="auth-button" disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
+          <div className="auth-link">
+            Don't have an account? <Link to="/register">Register</Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 import api from "../services/api";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "./Auth.css";
 
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setError("");
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      setLoading(false);
+      return;
+    }
     try {
+      setLoading(true);
       const response = await api.post("/auth/register", {
         name,
         email,
@@ -20,57 +29,78 @@ function Register() {
       });
 
       console.log(response.data);
+      setLoading(false);
       navigate("/");
     } catch (error) {
       console.log(error.response?.data);
+
+      setError(error.response?.data?.message || "Registration failed");
+
+      setLoading(false);
     }
   };
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1 className="auth-title">Register</h1>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name</label>
+        <form onSubmit={handleSubmit}>
+          <div className="auth-form-group">
+            <label>Name</label>
+            <br />
+            <input
+              className="auth-input"
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
           <br />
-          <input
-            type="text"
-            placeholder="Enter name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-
-        <br />
-
-        <div>
-          <label>Email</label>
+          <div className="auth-form-group">
+            <label>Email</label>
+            <br />
+            <input
+              className="auth-input"
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
           <br />
-          <input
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <br />
-
-        <div>
-          <label>Password</label>
+          <div className="auth-form-group">
+            <label>Password</label>
+            <br />
+            <input
+              className="auth-input"
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
           <br />
-          <input
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <br />
-
-        <button type="submit">Register</button>
-      </form>
+          <div className="auth-form-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              className="auth-input"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          {error && <p className="field-error">{error}</p>}
+          <br />{" "}
+          <button type="submit" className="auth-button" disabled={loading}>
+            {loading ? "Creating account..." : "Register"}
+          </button>
+          <div className="auth-link">
+            Already have an account? <Link to="/">Login</Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
